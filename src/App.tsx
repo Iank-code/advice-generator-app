c87649562749ceca6a098fd38925ed379c8c0e3f
Ian Kamau <kian99564@gmail.com>
2023-11-15 20:45:50 +0300

    Fetching data from advice api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import iconDice from "./assets/images/icon-dice.svg"
+import iconDice from "./assets/images/icon-dice.svg";
 
 function App() {
+  interface AdviceData {
+    id: number;
+    advice: string;
+  }
+  const API: string = "https://api.adviceslip.com/";
+  // advice
+  const [data, setData] = useState<AdviceData>();
+  useEffect(() => {
+    fetch(`${API}advice`)
+      .then((res) => {
+        if (!res.ok) throw new Error();
+        return res.json();
+      })
+      .then((data) => setData(data.slip));
+  }, []);
+
   return (
     <div className="advice-body">
-      <div className="container">
-        <h3>{`Advice #`}</h3>
-        <h1>{"Some text"}</h1>
+      {data && (
+        <div className="container">
+          <h3>{`Advice # ${data && data.id}`}</h3>
+          <h1>{data && data.advice}</h1>
 
-        <div className="divider-tab">
-          <hr className="divider" />
-          <div className="vertical-divider-tab">
-            <hr className="vertical-divider" />
-            <hr className="vertical-divider" />
+          <div className="divider-tab">
+            <hr className="divider" />
+            <div className="vertical-divider-tab">
+              <hr className="vertical-divider" />
+              <hr className="vertical-divider" />
+            </div>
+            <hr className="divider" />
           </div>
-          <hr className="divider" />
-        </div>
 
-        <div className="icon">
-          <img src={iconDice} alt="" />
+          <button className="icon">
+            <img src={iconDice} alt="" />
+          </button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
